e012b854119836fe42c8112876c7b113a8383a88
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-02-18 14:15:46 +0600

    Add helmet meta tag

--- src/client/pages/NotFoundPage.js
+++ src/client/pages/NotFoundPage.js
@@ -1,9 +1,26 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+
+const styles = {
+  marginTop: "80px"
+}
 
 const NotFoundPage = ({ staticContext = {} }) => {
   staticContext.notFound = true
 
-  return <h3 className="center-align">Oops! Route not found!!</h3>
+  const head = () => (
+    <Helmet>
+      <title>Page Not Found</title>
+      <meta property="og:title" content="Page Not Found" />
+    </Helmet>
+  )
+
+  return (
+    <div style={styles}>
+      {head()}
+      <h3 className="center-align">Oops! Route not found!!</h3>
+    </div>
+  )
 }
 
 export default {
